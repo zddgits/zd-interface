9d30269aac16e8334c09cb829daa034c6a0ffb67
zd <417256279@qq.com>
2023-03-20 14:20:18 +0800

    demo interface

--- zd-interface/src/main/java/com/zd/zdinterface/Main.java
+++ zd-interface/src/main/java/com/zd/zdinterface/Main.java
@@ -5,11 +5,13 @@ import com.zd.zdinterface.model.User;
 
 public class Main {
     public static void main(String[] args) {
-        ZdApiClient zdApiClient = new ZdApiClient();
+        String accessKey = "12345";
+        String secretKey = "abcdef";
+        ZdApiClient zdApiClient = new ZdApiClient(accessKey,secretKey);
         String res1 =  zdApiClient.getNameByGet("zd");
         String res2 =  zdApiClient.getNameByPost("zd");
         User user = new User();
-        user.setUsername("zd");
+        user.setUsername("张迪");
         String res3 =   zdApiClient.getUserNameByPost(user);
         System.out.println(res1);
         System.out.println(res2);
--- zd-interface/src/main/java/com/zd/zdinterface/controller/NameController.java
+++ zd-interface/src/main/java/com/zd/zdinterface/controller/NameController.java
@@ -1,7 +1,12 @@
 package com.zd.zdinterface.controller;
 
 import com.zd.zdinterface.model.User;
+import com.zd.zdinterface.utils.SignUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 查询名称
@@ -19,7 +24,33 @@ public class NameController {
         return "Post 你的名字是：" + name;
     }
     @PostMapping("/user")
-    public  String getUserNameByPost(@RequestBody User user) {
+    public  String getUserNameByPost(@RequestBody User user, HttpServletRequest request) throws UnsupportedEncodingException {
+        String accessKey = request.getHeader("accessKey");
+        String nonce = request.getHeader("nonce");
+        String timestamp = request.getHeader("timestamp");
+        String sign = request.getHeader("sign");
+        String body = new String(request.getHeader("body").getBytes(StandardCharsets.ISO_8859_1),StandardCharsets.UTF_8);
+        // todo 数据库中查是否已分配给用户
+        if (!accessKey.equals("12345")) {
+            throw new RuntimeException("无权限");
+        }
+        if (Long.parseLong(nonce) > 10000) {
+            throw new RuntimeException("无权限");
+        }
+//         todo 时间和当前时间不能超过 5 分钟
+        if (timestamp.isEmpty()) {
+
+        }
+//         todo 实际情况中是从数据库中查出 secretKey
+        String serverSign = SignUtils.genSign(body, "abcdef");
+        if (!sign.equals(serverSign)) {
+            throw new RuntimeException("无权限");
+        }
+//         todo 调用次数 + 1 invokeCount
+
+//        if(!accessKey.equals("12345")||!secretKey.equals("abcdef")){
+//            throw new RuntimeException("无权限");
+//        }
         return user.getUsername();
     }
 }
